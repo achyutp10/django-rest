a980b1be40e8d116226117bb492d6a6de0ed9074
achyut <achyutp10@gmail.com>
2024-06-01 19:15:56 +0545

    added search to user profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from profiles_api import serializers
 from rest_framework import viewsets
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from profiles_api import permissions
 
 class HelloApiView(APIView):
@@ -94,6 +95,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
   queryset = models.UserProfile.objects.all()
   authentication_classes = (TokenAuthentication,)
   permission_classes = (permissions.UpdateOwnProfile,)
+  filter_backends = (filters.SearchFilter,)
+  search_fields = ('name','email',)
+  
   
   
    
